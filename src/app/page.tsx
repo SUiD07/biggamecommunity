38b16e3b34b606308aa897c4d2b4c9dc4bc1b77a
Sirada U <nan.uthaiwat@gmail.com>
2025-07-09 20:44:22 +0700

    feat: add lower form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from 'next/link';
-import LowerForm from "../components/mine/LowerForm";
 import UpperForm from "../components/mine/UpperForm";
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
@@ -8,16 +7,10 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <>
-      <div>Hello world</div>
       <Link className="hover:font-bold" href="./FinishPage">
         Article
       </Link>
       <UpperForm />
-      <LowerForm />
-
-      <div>
-        <Button>Click me</Button>
-      </div>
     </>
   );
 }
--- src/components/mine/UpperForm.tsx
+++ src/components/mine/UpperForm.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import { Input } from '../ui/input'
+import React from "react";
+import { Input } from "../ui/input";
+import { LowerForm } from "./LowerForm";
 
 function UpperForm() {
   return (
-  <>
-  <div className ="columns-2 flex">
-    <img src="smcu.png" className="w-1/2"></img>
-    <div className="w-1/2 mt-20 mr-10">
-    <div className="text-3xl font-semibold flex-initial">Register With SMCU</div>
-    <div className="mt-3 mb-1">ชื่อ</div>
-    <Input></Input>
-    <div className="mt-3 mb-1">นามสกุล</div>
-    <Input></Input>
-    <div className="mt-3 mb-1">ชั้นปี</div>
-    <Input></Input>
-    </div>
-  </div>
-  </>
-  )
+    <>
+      <div className="columns-2 flex">
+        <img src="smcu.png" className="w-1/2"></img>
+        <div className="w-1/2 mt-20 mr-10">
+          <div className="text-3xl font-semibold flex-initial">
+            Register With SMCU
+          </div>
+          <div className="mt-3 mb-1">ชื่อ</div>
+          <Input></Input>
+          <div className="mt-3 mb-1">นามสกุล</div>
+          <Input></Input>
+          <div className="mt-3 mb-1">ชั้นปี</div>
+          <Input></Input>
+          <LowerForm/>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default UpperForm
+export default UpperForm;
