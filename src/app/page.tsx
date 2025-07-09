6c0d118bfe577fec76bf1414cf5bf06f0f31fa96
BLKdrone <sasisa.blink@gmail.com>
2025-07-09 18:48:53 +0700

    buttonfor nextpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import Link from 'next/link';
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>Hello world</div>
+    <>
+      <div>Hello world</div>
+      <Link className="hover:font-bold" href="./FinishPage">
+        Article
+      </Link>
+    </>
   );
 }
